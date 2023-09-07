00004a30348a2449cdfc0d5fd7b109ac5c86efe1
mwaney <emwaniki91@gmail.com>
2023-09-07 21:12:39 +0300

    Create basic HTML Structure of Page and Add data using JS

---
+++ app.js
@@ -0,0 +1,56 @@
+document.addEventListener("DOMContentLoaded", () => {
+  fetchData();
+  getCurrentTime();
+});
+
+const fetchData = () => {
+  const data = {
+    slackUserName: "edu",
+    slackDisplayImage:
+      "https://ca.slack-edge.com/T05FFAA91JP-U05R7BLPFQD-4f73341695cb-512",
+    currentDayOfTheWeek: getWeekDay(),
+    currentUTCTime: getCurrentTime(),
+    myTrack: "Frontend",
+    githubURL: "https://github.com/mwaney",
+  };
+
+  document.querySelector("[data-testid='slackUserName']").textContent =
+    data.slackUserName;
+  document
+    .querySelector("[data-testid='slackDisplayImage']")
+    .setAttribute("src", data.slackDisplayImage);
+  document
+    .querySelector("[data-testid='slackDisplayImage']")
+    .setAttribute("alt", data.slackUserName);
+  document.querySelector("[data-testid='currentDayOfTheWeek']").textContent =
+    data.currentDayOfTheWeek;
+  document.querySelector("[data-testid='myTrack']").textContent = data.myTrack;
+  document.querySelector("[data-testid='githubURL']").textContent =
+    data.githubURL;
+  document
+    .querySelector("[data-testid='githubURL']")
+    .setAttribute("href", data.githubURL);
+};
+
+function getWeekDay() {
+  const weekDays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  const dayNumber = new Date().getDay();
+  return weekDays[dayNumber];
+}
+
+function getCurrentTime() {
+  setInterval(() => {
+    const currentUTCTime = new Date().getTime();
+    document.querySelector('[data-testid="currentUTCTime"]').textContent =
+      currentUTCTime;
+  }, 1000);
+}
